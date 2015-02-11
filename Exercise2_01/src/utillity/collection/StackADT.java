e048aed3827369445973b627102643e09e415ca0
jimmi280586 <joa@jimmiandersen.dk>
2015-02-11 09:59:20 +0100

    exercise2_01 complete

--- Exercise2_01/src/utillity/collection/StackADT.java
+++ Exercise2_01/src/utillity/collection/StackADT.java
@@ -2,10 +2,11 @@ package utillity.collection;
 
 public interface StackADT<T>
 {
-	public void push(T element);
+	
 	public T pop();
 	public T peek();
 	public int indexOf(T element);
 	public boolean isEmpty();
 	public int size();
+	public void push(T element);
 }
