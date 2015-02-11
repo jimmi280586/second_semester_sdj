c84f12bfa96b5fdf8f9eb3687c14dedd7cb942af
jimmi280586 <joa@jimmiandersen.dk>
2015-02-11 09:21:29 +0100

    exercise 2

---
+++ Exercise2_01/src/utillity/collection/ArrayStack.java
@@ -0,0 +1,6 @@
+package utillity.collection;
+
+public class ArrayStack<T> implements StackADT
+{
+
+}
---
+++ Exercise2_01/src/utillity/collection/StackADT.java
@@ -0,0 +1,11 @@
+package utillity.collection;
+
+public interface StackADT<T>
+{
+	public void push(T element);
+	public T pop();
+	public T peek();
+	public int indexOf(T element);
+	public boolean isEmpty();
+	public int size();
+}
