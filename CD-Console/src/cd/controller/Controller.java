54ddb2662f5544778e3f9178927aaf22b33588db
jimmi280586 <joa@jimmiandersen.dk>
2015-02-12 13:39:56 +0100

    exercise1 finished

--- CD-Console/src/cd/controller/Controller.java
+++ CD-Console/src/cd/controller/Controller.java
@@ -10,17 +10,19 @@ public class Controller
 {
 	private CdModel model;
 	private CdView view;
-	
+		
 	public Controller(CdModel model, CdView view)
 	{
 		this.model = model;
 		this.view = view;
 	}
 
-	public void execute(String what)
+	public void execute(String input)
 	{
-		switch (what)
+		
+		switch (input)
 		{
+			
 			case "List":
 				this.view.show("" + this.model.getAll());
 				break;
@@ -28,36 +30,46 @@ public class Controller
 				this.view.show("Add method - not implemented");
 				break;
 			case "Remove":
-				String input = this.view.get("Title");
-				if (input.length() == 0)
+				String message;
+				String input1 = this.view.get("Title");
+				if (input1.length() == 0)
+				{
+					
+					message = "";
 					return;
-				String msg = "";
-				Cd cd = this.model.removeCD(input);
+					
+				}
+				Cd cd = this.model.removeCD(input1);
 				if (cd != null)
 				{
-					msg = "REMOVED: \n" + cd.toString();
+					message = "REMOVED: \n" + cd.toString();
 				}
 				else
 				{
-					msg = "No CD with title: \"" + input + "\" found";
+					message = "No CD with title: \"" + input1 + "\" found";
 				}
-				this.view.show(msg);
+				this.view.show(message);
 				break;
 			case "Search":
-				input = this.view.get("Title");
-				if (input == null)
+				String message1 = "";
+				input1 = this.view.get("Title");
+				if (input1 == null)
+				{
+					message1 = "";
 					return;
-				msg = "";
-				CdList list = this.model.getCD(input);
+					
+				}
+				CdList list = this.model.getCD(input1);
+				
 				for (int i = 0; i < list.getNumberOfCds(); i++)
 				{
-					msg += list.getCD(i) + "\n";
+					message1 += list.getCD(i) + "\n";
 				}
 				if (list.getNumberOfCds() == 0)
 				{
-					msg = "No CD with title: \"" + input + "\" found";
+					message = "No CD with title: \"" + input1 + "\" found";
 				}
-				this.view.show(msg);
+				this.view.show(message1);
 				break;
 			case "Quit":
 				System.out.println("Quit");
@@ -66,6 +78,7 @@ public class Controller
 				this.view.show("Wrong input");
 				break;
 		}
+		
 	}
 
 	
